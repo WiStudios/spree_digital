bfc5ac01dd9bf1feb9aeb3ada14187c38fa379fc
Satyakam Pandya <satyakamp@nividata.com>
2021-09-13 14:18:39 +0530

    Removed reset downloads button in order tabs

--- app/overrides/spree/admin/shared/_order_submenu/add_digital_versions_to_admin_product_tabs.rb
+++ app/overrides/spree/admin/shared/_order_submenu/add_digital_versions_to_admin_product_tabs.rb
@@ -1,12 +1,13 @@
-Deface::Override.new(
-  virtual_path: 'spree/admin/shared/_order_tabs',
-  name: 'add_digital_versions_to_admin_product_tabs',
-  insert_bottom: '[data-hook="admin_order_tabs"]',
-  text: <<-HTML
-          <% if can?(:update, @order) && @order.some_digital? %>
-            <li>
-              <%= link_to_with_icon 'icon-cloud', Spree.t(:reset_downloads, scope: 'digitals'), reset_digitals_admin_order_url(@order) %>
-            </li>
-          <% end %>
-        HTML
-)
+# TODO: Commented below code to hide reset digitals button
+# Deface::Override.new(
+#   virtual_path: 'spree/admin/shared/_order_tabs',
+#   name: 'add_digital_versions_to_admin_product_tabs',
+#   insert_bottom: '[data-hook="admin_order_tabs"]',
+#   text: <<-HTML
+#           <% if can?(:update, @order) && @order.some_digital? %>
+#             <li>
+#               <%= link_to_with_icon 'icon-cloud', Spree.t(:reset_downloads, scope: 'digitals'), reset_digitals_admin_order_url(@order) %>
+#             </li>
+#           <% end %>
+#         HTML
+# )
